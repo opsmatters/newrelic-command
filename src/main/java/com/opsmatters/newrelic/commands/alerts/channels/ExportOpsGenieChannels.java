a519f9f2e591847e47a81e075d22390b77071c0f
opsdemon <gerald_curley@hotmail.com>
2018-02-26 12:24:56 +0000

    Added import and export commands for the rest of the alert channels

---
+++ src/main/java/com/opsmatters/newrelic/commands/alerts/channels/ExportOpsGenieChannels.java
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2018 Gerald Curley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.opsmatters.newrelic.commands.alerts.channels;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+import org.apache.commons.cli.CommandLine;
+import com.opsmatters.newrelic.api.model.alerts.channels.OpsGenieChannel;
+import com.opsmatters.newrelic.batch.AlertManager;
+import com.opsmatters.newrelic.batch.model.AlertConfiguration;
+import com.opsmatters.core.documents.Workbook;
+import com.opsmatters.newrelic.commands.Opt;
+import com.opsmatters.newrelic.commands.BaseCommand;
+
+/**
+ * Implements the New Relic command line option to export a set of OpsGenie alert channels.
+ * 
+ * @author Gerald Curley (opsmatters)
+ */
+public class ExportOpsGenieChannels extends BaseCommand
+{
+    private static final Logger logger = Logger.getLogger(ExportOpsGenieChannels.class.getName());
+    private static final String NAME = "export_opsgenie_channels";
+
+    private String filename;
+    private String worksheet;
+    private boolean append = false;
+
+    /**
+     * Default constructor.
+     */
+    public ExportOpsGenieChannels()
+    {
+        options();
+    }
+
+    /**
+     * Returns the name of the command.
+     * @return The name of the command
+     */
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * Sets the options for the command.
+     */
+    @Override
+    protected void options()
+    {
+        super.options();
+        addOption(Opt.FILE, "The name of the file to export the alert channels to");
+        addOption(Opt.SHEET);
+        addOption(Opt.APPEND);
+    }
+
+    /**
+     * Parse the command-specific options.
+     * @param cli The parsed command line
+     */
+    protected void parse(CommandLine cli)
+    {
+        // Filename option
+        if(hasOption(cli, Opt.FILE, true))
+        {
+            filename = getOptionValue(cli, Opt.FILE);
+            logOptionValue(Opt.FILE, filename);
+        }
+
+        // Sheet option
+        if(hasOption(cli, Opt.SHEET, false))
+        {
+            worksheet = getOptionValue(cli, Opt.SHEET);
+            logOptionValue(Opt.SHEET, worksheet);
+        }
+
+        // Append option
+        if(hasOption(cli, Opt.APPEND, false))
+        {
+            append = true;
+        }
+    }
+
+    /**
+     * Export the OpsGenie alert channels.
+     */
+    protected void execute()
+    {
+        AlertManager manager = new AlertManager(getApiKey());
+        AlertConfiguration config = new AlertConfiguration();
+
+        // Get the channels
+        config.setAlertChannels(manager.getAlertChannels());
+        List<OpsGenieChannel> channels = config.getOpsGenieChannels();
+
+        try
+        {
+            Workbook workbook = null;
+            if(append)
+                workbook = Workbook.getWorkbook(new File(filename));
+            manager.writeOpsGenieChannels(channels, filename, worksheet, 
+                new FileOutputStream(filename), workbook);
+        }
+        catch(IOException e)
+        {
+            logger.severe("Unable to write alert channel file: "+e.getClass().getName()+": "+e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/opsmatters/newrelic/commands/alerts/channels/ImportCampfireChannels.java
@@ -0,0 +1,124 @@
+/*
+ * Copyright 2018 Gerald Curley
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.opsmatters.newrelic.commands.alerts.channels;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.Logger;
+import org.apache.commons.cli.CommandLine;
+import com.opsmatters.newrelic.batch.AlertManager;
+import com.opsmatters.newrelic.batch.model.AlertConfiguration;
+import com.opsmatters.newrelic.commands.Opt;
+import com.opsmatters.newrelic.commands.BaseCommand;
+
+/**
+ * Implements the New Relic command line option to import a set of Campfire alert channels.
+ * 
+ * @author Gerald Curley (opsmatters)
+ */
+public class ImportCampfireChannels extends BaseCommand
+{
+    private static final Logger logger = Logger.getLogger(ImportCampfireChannels.class.getName());
+    private static final String NAME = "import_campfire_channels";
+
+    private String filename;
+    private String worksheet;
+    private boolean delete = false;
+
+    /**
+     * Default constructor.
+     */
+    public ImportCampfireChannels()
+    {
+        options();
+    }
+
+    /**
+     * Returns the name of the command.
+     * @return The name of the command
+     */
+    public String getName()
+    {
+        return NAME;
+    }
+
+    /**
+     * Sets the options for the command.
+     */
+    @Override
+    protected void options()
+    {
+        super.options();
+        addOption(Opt.FILE, "The name of the file containing alert channels");
+        addOption(Opt.SHEET);
+        addOption(Opt.DELETE, "For import files, delete any existing alert channel with that name before creating the new alert channel");
+    }
+
+    /**
+     * Parse the command-specific options.
+     * @param cli The parsed command line
+     */
+    protected void parse(CommandLine cli)
+    {
+        // Filename option
+        if(hasOption(cli, Opt.FILE, true))
+        {
+            filename = getOptionValue(cli, Opt.FILE);
+            logOptionValue(Opt.FILE, filename);
+        }
+
+        // Sheet option
+        if(hasOption(cli, Opt.SHEET, false))
+        {
+            worksheet = getOptionValue(cli, Opt.SHEET);
+            logOptionValue(Opt.SHEET, worksheet);
+        }
+
+        // Delete option
+        if(hasOption(cli, Opt.DELETE, false))
+        {
+            delete = true;
+        }
+    }
+
+    /**
+     * Import the Campfire alert channels.
+     */
+    protected void execute()
+    {
+        AlertManager manager = new AlertManager(getApiKey());
+        AlertConfiguration config = new AlertConfiguration();
+
+        try
+        {
+            // Read the alert channels
+            config.addAlertChannels(manager.readCampfireChannels(filename, worksheet, 
+                new FileInputStream(filename)));
+        }
+        catch(IOException e)
+        {
+            logger.severe("Unable to read alert channel file: "+e.getClass().getName()+": "+e.getMessage());
+        }
+
+        // Delete the existing alert channels
+        if(delete)
+            manager.deleteAlertChannels(config.getAlertChannels());
+
+        // Create the new alert channels
+        manager.createAlertChannels(config.getAlertChannels());
+    }
+}
